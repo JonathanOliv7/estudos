c44c85c52701cb0e95099f1f712e16610aad7d78
Jonathan Oliveira <jonathanoliv7@gmail.com>
2025-11-23 22:48:28 -0300

    Iniciando o jogo trivia game

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module trivia_game {
+}
---
+++ src/trivia_game/App.java
@@ -0,0 +1,10 @@
+package trivia_game;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
