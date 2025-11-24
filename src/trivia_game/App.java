995c12d128a391a429cef0c7a5bfea447be1491c
Jonathan Oliveira <jonathanoliv7@gmail.com>
2025-11-24 12:34:02 -0300

    Criado as opções de tema e inicio das perguntas com pontuação

--- src/trivia_game/App.java
+++ src/trivia_game/App.java
@@ -12,7 +12,6 @@ public class App {
 		System.out.print("Digite sua nova senha:");
 		String password = sc.nextLine();
 		
-		System.out.print("testando variaveis" + password + login);
 		
 		int attempts = 0;
 		
@@ -26,16 +25,83 @@ public class App {
 			String key = sc.nextLine();
 			
 			if(login.equals(user) && password.equals(key)) {
+				System.out.println("Login efetuado com sucesso!!!");
 				int verification = 0;
-					while(verification != 2) {
-						System.out.println("Login efetuado com sucesso!!!");
-						System.out.print("VAMOS INICIAR O GAME");
+					while(verification == 0) {
+						
+						System.out.println("VAMOS INICIAR O GAME"); 
+						System.out.println("Escolha um tema, digite o número correspondente ao tema:");
+						System.out.println("1-Animais\n"+"2-Matemática\n"+"3-Geografia\n"+"4-Portugês");
+						System.out.print("Digite a opção desejada:"); 
+						char teme = sc.next().charAt(0);
+						int pontuation = 0;
+						switch(teme) {
+							case '1':
+								System.out.println("Vale 5 pontos"); 
+								System.out.println("1-Nível Fácil: Qual classe de vertebrados é a única capaz de voar "
+										+ "ativamente e é caracterizada por ter o corpo coberto de penas?"); 
+								System.out.print("a)Aves\n"
+										+ "b)Mamíferos\n"
+										+ "c)répteis\n"
+										+ "d)Anfíbios\n");
+								System.out.println("Digite a alternativa correta?"); 
+								char question1 = sc.next().charAt(0);
+								
+								switch(question1) {
+								case 'a':
+									pontuation += 5;
+									break;
+								default:
+									break;
+								}
+								System.out.println("Vale 10 pontos"); 
+								System.out.println("1-Nível Fácil: Qual classe de vertebrados é a única capaz de voar "
+										+ "ativamente e é caracterizada por ter o corpo coberto de penas?"); 
+								System.out.print("a)Aves\n"
+										+ "b)Mamíferos\n"
+										+ "c)répteis\n"
+										+ "d)Anfíbios\n");
+								System.out.println("Digite a alternativa correta?"); 
+								char question2 = sc.next().charAt(0);
+								
+								switch(question2) {
+								case 'a':
+									pontuation += 10;
+									break;
+								default:
+									break;
+								}
+								
+								
+								
+								
+								
+								
+								
+								
+									
+								break;
+							case '2':	
+								System.out.println("deu certo 2"); 
+								break;
+							case '3':
+								System.out.println("deu certo 3"); 
+								break;
+							case '4':
+								System.out.println("deu certo 4"); 
+								break;
+							default:
+								break;
+									}											
+						
+					
 						
 					}
 			}else
-				System.out.print("Senha inválida");
+				System.out.println("Senha ou usúario inválido.");
 		}
 		
+		sc.close();
 	}
 
 }
